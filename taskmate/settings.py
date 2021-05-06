29b8b8cc32b91be5b866bb170df6109121f422aa
stali1234 <stalin.sangeethraj@gmail.com>
2021-05-06 16:39:06 +0530

    updated env files

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,3 +137,6 @@ LOGIN_REDIRECT_URL = "todolist"
 
 
 LOGIN_URL = 'login'
+
+
+django_heroku.settings(locals())
